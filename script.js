53815dd7b5a732d95f1f4201388d09a22541fe5d
InspiredProgrammer <emtfireman1921@gmail.com>
2025-12-11 14:15:46 -0600

    more JS repetition practice

--- script.js
+++ script.js
@@ -16,8 +16,61 @@ const twoSum = function(nums, target) {
         for (let j = i + 1; j < nums.length; j++) {
             if (nums[i] + nums[j] === target) {
                 return [ i, j];
+
+                console.log(twoSum(nums, target));
             }
         }
     }
 }
-console.log(twoSum(nums, target));
+
+const colors = ['red' , 'green' , 'blue'];
+
+for (let i =  0; i < colors.length; i++) {
+    console.log(colors[i]);
+}
+
+
+for (const color of colors) {
+    console.log(color);
+}
+
+
+colors.forEach(function(color) {
+    console.log(color);
+});
+
+
+const animals = ['cat' , 'dog' , 'rabbit'];
+
+let i = 0;
+while (i< animals.length) {
+    console.log(animals[i]);
+    i++;
+}
+
+
+// Function creating an array
+function createNumberArray(start, end) {
+    const result = [];
+    for (let i = start; i <= end; i++) {
+        result.push(i);
+    }
+    return result;
+}
+
+const years = createNumberArray(1900, 2025);
+console.log(years);
+
+
+// Function filters and array
+function getEvenNumbers(numbers) {
+    const evens = [];
+    for (const num of numbers) {
+        if (num % 2 === 0) {
+            evens.push(num);
+        }
+    }
+    return evens;
+}
+
+console.log(getEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
